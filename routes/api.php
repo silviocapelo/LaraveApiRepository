1584708d9941ac28ad74ee4a727bba801c9b0072
silviocapelo <silvio.silva51@gmail.com>
2023-12-02 14:23:57 -0300

    Adicionado rotas api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+    Route::get('orders', [PostController::class, 'index']);
+    Route::get('orders/{id}', [PostController::class, 'show']);
+    Route::post('orders', [PostController::class, 'store']);
+    Route::put('orders/{id}', [PostController::class, 'update']);
+    Route::delete('orders/{id}', [PostController::class, 'delete']);
